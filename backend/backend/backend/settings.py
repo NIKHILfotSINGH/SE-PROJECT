5c14e1d17363250fc06a6352389471d9f11049fa
nikhil <nikhilsinghdufot@gmail.com>
2026-04-21 06:48:29 +0530

    changed some vercel configurations

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3k8!5bov)mruy$^fm#o*z$+)lr56v#+5insu3h5p4^hfgs%$3w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
